4184371585fcfaf3f54822ed579af1de9dc78644
Jimmy Byrum <me@jimmybyrum.com>
2011-09-16 23:59:34 -0700

    updating demo

--- demo.js
+++ demo.js
@@ -8,34 +8,34 @@ function addEvent(element, action, callback, bubble) {
 }
 
 function xhr(_type, _format, _url, _params, _callback, _failure) {
-	_type = _type || "GET";
-	try {
-		var _xhr = new XMLHttpRequest();
-		_xhr.open(_type, _url, true);
-		if (_type === "POST") {
-			//Send the proper header information along with the request
-			_xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			_xhr.setRequestHeader("Content-length", _params.length);
-			_xhr.setRequestHeader("Connection", "close");
-			_xhr.send(_params);
-		} else {
-			_xhr.send();
-		}
-		_xhr.onreadystatechange = function() {
-			if (_xhr.readyState === 4) {
-				var _response = _xhr.responseText;
-				if (_format==="json" && _response!=="") {
-					_response = eval("(" + _xhr.responseText + ")");
-				}
-				_callback(_response);
-			}
-		}
-	} catch (e) {
-		console.debug("error: ", e);
-		if (typeof(_failure) === "function") {
-			_failure();
-		}
-	}
+    _type = _type || "GET";
+    try {
+        var _xhr = new XMLHttpRequest();
+        _xhr.open(_type, _url, true);
+        if (_type === "POST") {
+            //Send the proper header information along with the request
+            _xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            _xhr.setRequestHeader("Content-length", _params.length);
+            _xhr.setRequestHeader("Connection", "close");
+            _xhr.send(_params);
+        } else {
+            _xhr.send();
+        }
+        _xhr.onreadystatechange = function() {
+            if (_xhr.readyState === 4) {
+                var _response = _xhr.responseText;
+                if (_format==="json" && _response!=="") {
+                    _response = eval("(" + _xhr.responseText + ")");
+                }
+                _callback(_response);
+            }
+        }
+    } catch (e) {
+        console.debug("error: ", e);
+        if (typeof(_failure) === "function") {
+            _failure();
+        }
+    }
 };
 
 var _search = document.getElementById("search");
@@ -44,32 +44,33 @@ var _results = document.getElementById("results");
 var _submit = document.getElementById("submit");
 
 addEvent(_search, "submit", function(e) {
-	e.preventDefault();
-	_results.innerHTML = "Results for: " + _input.value;
-	Crosshatch.setLocation("/search/"+_input.value+"/");
-	Crosshatch.setTitle("Search results for " + _input.value);
+    e.preventDefault();
+    _results.innerHTML = "Results for: " + _input.value;
+    Crosshatch.setLocation("/search/"+_input.value+"/");
+    Crosshatch.setTitle("Search results for " + _input.value);
 }, false);
 function doSearch(_term) {
-	_input.value = _term;
-	_submit.click();
+    _input.value = _term;
+    _submit.click();
 }
 function clearSearch() {
-	_input.value = "";
-	_input.focus();
-	_results.innerHTML = "";
-	Crosshatch.setTitle("Search");
+    _input.value = "";
+    _input.focus();
+    _results.innerHTML = "";
+    Crosshatch.setTitle("Search");
 }
 
 function loadSettings(_url) {
-	// fetch a page via ajax
-	var _exists = document.getElementById("settings");
-	if (_exists === null) {
-		xhr("GET", "html", _url, {}, function(_response) {
-			var _html = document.createElement("div");
-			_html.innerHTML = _response;
-			document.body.appendChild(_html.childNodes[0]);
-		});
-	}
-	document.body.className = "settings";
-	Crosshatch.setTitle("Settings");
+    // fetch a page via ajax
+    var _exists = document.getElementById("settings");
+    if (_exists === null) {
+        xhr("GET", "html", _url, {}, function(_response) {
+            var _html = document.createElement("div");
+            _html.innerHTML = _response;
+            document.body.appendChild(_html.childNodes[0]);
+        });
+    }
+    document.body.className = "settings";
+    Crosshatch.setTitle("Settings");
 }
+
--- urls.js
+++ urls.js
@@ -1,44 +1,48 @@
 Crosshatch.route({
-	url: "/",
-	pattern: /^(\/|\/#|)$/,
-	controller: function(_self, _url) {
-		document.body.className = "home";
-		Crosshatch.setTitle("Home");
-	}
+    url: "/",
+    pattern: /^(\/|\/#|)$/,
+    controller: function(_self, _url) {
+        document.body.className = "home";
+        Crosshatch.setTitle("Home");
+    }
 });
 
 Crosshatch.route({
-	url: "/search/",
-	pattern: /^\/search\/$/,
-	controller: function(_self, _url) {
-		document.body.className = "search";
-		clearSearch();
-	}
+    url: "/search/",
+    pattern: /^\/search\/$/,
+    controller: function(_self, _url) {
+        document.body.className = "search";
+        clearSearch();
+    }
 });
 
 Crosshatch.route({
-	url: "/search/{term}/",
-	pattern: /^\/search\/([\d\w\s+_\-%,"']*)\/$/,
-	controller: function(_self, _url) {
-		document.body.className = "search";
-		// get the search term from the url
-		var _term = _url.replace(_self.pattern, "$1");
-		doSearch(_term);
-	}
+    url: "/search/{term}/",
+    pattern: /^\/search\/([\d\w\s+_\-%,"']*)\/$/,
+    controller: function(_self, _url) {
+        document.body.className = "search";
+        // get the search term from the url
+        var _term = _url.replace(_self.pattern, "$1");
+        _term = _term.replace(/\+/g, "%20");
+        _term = _term.replace(/\//g, "%2F");
+        _term = decodeURI(_term);
+        doSearch(_term);
+    }
 });
 
 Crosshatch.route({
-	url: "/settings/",
-	pattern: /^\/settings\/$/,
-	controller: function(_self, _url) {
-		loadSettings("settings.html");
-	}
+    url: "/settings/",
+    pattern: /^\/settings\/$/,
+    controller: function(_self, _url) {
+        loadSettings("settings.html");
+    }
 });
 
 Crosshatch.route({
-	url: "/github/",
-	pattern: /^\/github\/$/,
-	controller: function(_self, _url) {
-		document.body.className = "github";	
-	}
+    url: "/github/",
+    pattern: /^\/github\/$/,
+    controller: function(_self, _url) {
+        document.body.className = "github"; 
+    }
 });
+
